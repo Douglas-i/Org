f173e92ce7aaae466e92010816788727874ed782
Douglas-i <hdouglasu@gmail.com>
2023-04-12 09:29:20 -0600

    Creacion del Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header/Header';
+// console.log(Header);
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">      
+      <Header></Header>      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css"
+
+function Header() {
+    return <header className="header">
+            <img src="/img/Header.png" alt="org"/>
+        </header>        
+
+}
+
+export default Header //Hay que exportarlo para luego importarlo en el app.js
