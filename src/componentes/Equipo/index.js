a3dc1d876f82837426c7663abd8c135e8fdc2205
Douglas-i <hdouglasu@gmail.com>
2023-04-23 21:58:54 -0600

    Colores a las tarjetas de los colaboradores

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -16,7 +16,7 @@ const Equipo = (props) => {
             <h3 style={{borderColor: colorPrimario}}>{titulo}</h3>
             <div className='colaboradores'>            
                 {
-                    colaboradores.map( (colaborador, index) => <Colaborador datos={colaborador} key={index}/>)
+                    colaboradores.map( (colaborador, index) => <Colaborador datos={colaborador} key={index} colorPrimario={colorPrimario}/>)
                 }
             </div>
             </section>
